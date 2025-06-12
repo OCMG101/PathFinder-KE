6ec30dff0997b96c7e4957c74728625334cd39bd
Brian <briankinyua0101@gmail.com>
2025-06-12 11:21:35 +0300

    add the home routing

--- pathfinder/src/App.jsx
+++ pathfinder/src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import About from './views/About';
 import Contact from './views/Contact';
-
+import Home from './views/Home';
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<h1>PathFinder KE</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
