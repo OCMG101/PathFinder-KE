98563f13b493b8b3fc3b9b51356d068b7d5be963
Brian <briankinyua0101@gmail.com>
2025-06-12 11:07:55 +0300

    add routing

--- pathfinder/src/App.jsx
+++ pathfinder/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import About from './views/About';
+import Contact from './views/Contact';
 
+
+function App() {
   return (
-    <>
-     <h1>PathFinder KE</h1>
-    </>
-  )
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<h1>PathFinder KE</h1>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
