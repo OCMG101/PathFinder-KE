f6b464ef6ca232704af13afad6cce9b1587d0124
Brian <briankinyua0101@gmail.com>
2025-06-12 11:21:14 +0300

    add a footer

---
+++ pathfinder/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+const Footer = () => (
+  <Box
+    component="footer"
+    sx={{
+      py: 3,
+      px: 2,
+      mt: 'auto',
+      backgroundColor: (theme) =>
+        theme.palette.mode === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+      textAlign: 'center'
+    }}
+  >
+    <Container maxWidth="md">
+      <Typography variant="body2" color="text.secondary">
+        © {new Date().getFullYear()} PathFinder-KE. All rights reserved.
+      </Typography>
+    </Container>
+  </Box>
+);
+
+export default Footer;
